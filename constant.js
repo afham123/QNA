24d6bf005e1c62d117c5a2721665d0173a08884a
Afham Fardeen <124774184+AfhamFardeen@users.noreply.github.com>
2024-07-26 12:17:31 +0530

    initital commit

---
+++ constant.js
@@ -0,0 +1,7 @@
+const imageUrls =
+    [
+        "https://media.giphy.com/media/10SvWCbt1ytWCc/giphy.gif",
+        "https://media.giphy.com/media/LwIyvaNcnzsD6/giphy.gif",
+    ]
+
+module.exports = imageUrls;
---
+++ index.js
@@ -0,0 +1,62 @@
+const imageUrls =
+    [
+        "https://media.giphy.com/media/10SvWCbt1ytWCc/giphy.gif",
+        "https://media.giphy.com/media/LwIyvaNcnzsD6/giphy.gif",
+    ];
+
+function main() {
+    console.log("hello world");
+    const data = {
+        title: 'Card Title1',
+        description: 'This is a sample card description.',
+        img: 'https://example.com/image.jpg',
+        link: 'https://example.com'
+    }
+
+    const newDiv = document.createElement("div");
+    // and give it some content
+    const newContent = document.createTextNode("Hi there and greetings!");
+    // add the text node to the newly created div
+    newDiv.appendChild(newContent);
+    console.log(imageUrls);
+
+    appendCard(4, "https://media.giphy.com/media/LwIyvaNcnzsD6/giphy.gif", "My title")
+}
+main();
+
+function appendCard(n, imgUrl, CardTitle) {
+    const card = document.createElement('div');
+    card.className = `card ${n}`;
+
+    // Create the card_image div
+    const cardImage = document.createElement('div');
+    cardImage.className = 'card_image';
+
+    // Create the img element and set its src attribute
+    const img = document.createElement('img');
+    img.src = imgUrl;
+
+    // Append the img element to the card_image div
+    cardImage.appendChild(img);
+
+    // Create the card_title div
+    const cardTitle = document.createElement('div');
+    cardTitle.className = 'card_title title-white';
+
+    // Create the p element and set its text content
+    const p = document.createElement('p');
+    p.textContent = CardTitle;
+
+    // Append the p element to the card_title div
+    cardTitle.appendChild(p);
+
+    // Append the card_image and card_title divs to the main card div
+    card.appendChild(cardImage);
+    card.appendChild(cardTitle);
+
+    // Append the main card div to the body
+    document.getElementsByClassName("cards-list")[0].appendChild(card);
+}
+
+
+
